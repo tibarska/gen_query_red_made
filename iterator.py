fbb5ac39ec9940b6098f991be79898fb30907c5f
Nikita Orlov <nikovtb1@gmail.com>
2023-04-24 18:41:23 +0300

    Update iterator.py
    added shuffling data

--- iterator.py
+++ iterator.py
@@ -1,12 +1,14 @@
 import sys
+import random
 import sqlite3
 from utils import DB_PATH
 
 
 class DBIterator:
-    def __init__(self, table, batch_size=1, db_path=DB_PATH):
+    def __init__(self, table, batch_size=1, shuffle=False, db_path=DB_PATH):
         self.con = sqlite3.connect(db_path)
         self.table = table
+        self.shuffle = shuffle
         self.batch_size = batch_size
         self.cursor = self.con.cursor()
         self.min, self.max = self.cursor.execute(
@@ -15,10 +17,17 @@ class DBIterator:
 
     def __iter__(self):
         self.curr_id = self.min
+        self.ids = list(range(self.min, self.max + 1))
+        if self.shuffle:
+            random.shuffle(self.ids)
         return self
 
     def make_request(self, start_pos, end_pos):
-        sql = f"SELECT * FROM {self.table} WHERE ID BETWEEN {start_pos} and {end_pos}"
+        sample_ids = tuple(self.ids[start_pos: end_pos + 1])
+        if len(sample_ids) != 1:
+            sql = "SELECT * FROM {} WHERE ID IN {}".format(self.table, sample_ids)
+        else:
+            sql = "SELECT * FROM {} WHERE ID = {}".format(self.table, sample_ids[0])
         data = self.cursor.execute(sql).fetchall()
         assert (
             len(data) == end_pos - start_pos + 1
@@ -39,6 +48,8 @@ if __name__ == "__main__":
     # table_name = 'docs'
     table_name = sys.argv[1]
     bs = int(sys.argv[2])
-    test_iterator = DBIterator(table_name, bs)
+    shuffle_data = True if sys.argv[3] == 'True' else False
+
+    test_iterator = DBIterator(table_name, bs, shuffle_data)
     for batch in test_iterator:
         print(batch)
